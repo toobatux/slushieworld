660e94b650c315753194c53a27a36175d6290372
toobatux <toobatux@gmail.com>
2024-05-02 19:29:56 -0400

    fix

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { CartPage } from "./pages/CartPage.js";
 
 import { Amplify } from 'aws-amplify';
 import LoginPage from "./pages/LoginPage.js";
-import awsExports from './aws-exports';
+import awsExports from './aws-exports.js';
 Amplify.configure(awsExports);
 
 function App() {
